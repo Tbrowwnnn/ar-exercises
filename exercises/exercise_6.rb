39dc11c65a64f3596c7298ed91d305ec878553c1
Tyler Brown <tyler.brown@telus.com>
2023-01-23 20:11:38 -0700

    update exercise 6 complete

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,4 +8,8 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
-# Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Tyler", last_name: "Brown", hourly_rate: 600)
+@store1.employees.create(first_name: "Darth", last_name: "Vader", hourly_rate: 800000)
+@store2.employees.create(first_name: "Geralt", last_name: "of Rivia", hourly_rate: 7)
+@store2.employees.create(first_name: "Han", last_name: "Solo", hourly_rate: 20000)
