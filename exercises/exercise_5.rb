27f7c9475484890c23b13812c7a35212f5ddd5d2
Tyler Brown <tyler.brown@telus.com>
2023-01-23 20:04:23 -0700

    update exercise 5 complete

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -7,4 +7,10 @@ require_relative './exercise_4'
 puts "Exercise 5"
 puts "----------"
 
-# Your code goes here ...
+@total_rev = Store.sum("annual_revenue")
+@annual_rev = Store.average("annual_revenue")
+@best_stores = Store.where("annual_revenue >= 1000000").count
+
+puts "Total revenue is #{@total_rev}"
+puts "AAV is #{@annual_rev}"
+puts "The number of stores generating over $1 mil is #{@best_stores}"
